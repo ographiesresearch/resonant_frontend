997da86c6bd4c8e841112d6bdab848d50aa2a045
Eric Robsky Huntley <ehuntley@mit.edu>
2023-09-02 14:33:42 -0400

    main page button fix

--- src/lib/components/WelcomeButton.svelte
+++ src/lib/components/WelcomeButton.svelte
@@ -2,4 +2,4 @@
     export let text = "Is My Project or Community Eligible?"
 </script>
 
-<button class="button block is-fullwidth has-background-primary has-text-dark has-text-weight-bold shadow" href="/map">{text}</button>
+<a class="button block is-fullwidth has-background-primary has-text-dark has-text-weight-bold shadow" href="/map">{text}</a>
