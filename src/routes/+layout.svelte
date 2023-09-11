e3b01dad7582c137395de7553f3c8d648a89ee35
Eric Robsky Huntley <ehuntley@mit.edu>
2023-09-11 19:28:16 -0400

    fix favicon

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,7 +2,11 @@
     import Navbar from '$lib/components/Navbar/Navbar.svelte';
     import site_data from '$lib/config/instance.json';
     import "$lib/styles/style.css";
+    import favicon from '$lib/favicon.png';
 </script>
+<svelte:head>
+  <link rel="icon shortcut" href={favicon}>
+</svelte:head>
 <title>{site_data.title}</title>
 <Navbar pages={site_data.pages}/>
 <slot />
