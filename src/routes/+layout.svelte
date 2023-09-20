e57ea82acc6d96222db2cb5ae94034d5491447ad
Eric Robsky Huntley <ehuntley@mit.edu>
2023-09-19 21:25:15 -0400

    google tag

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,6 +5,9 @@
     import favicon from '$lib/favicon.png';
 </script>
 <svelte:head>
+  <!-- Google tag (gtag.js) --> <script async src="https://www.googletagmanager.com/gtag/js?id=G-QWSCPX7GJY"></script> <script> window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments);} gtag('js', new Date()); gtag('config', 'G-QWSCPX7GJY'); </script>
+  <meta name="viewport" content="initial-scale=1,maximum-scale=1,user-scalable=no" />
+  <meta charset="utf-8" />
   <link rel="icon shortcut" href={favicon}>
   <title>{site_data.title}</title>
 </svelte:head>
